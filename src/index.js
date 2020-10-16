b5461f152d2dda0d83758a6f7bfed49c6c97ee13
amartuvshin <a.amartuvshin@e-logit.com>
2020-10-16 14:39:16 +0900

    fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import initTilt from "./js/tilt";
 import initSr from "./js/sr";
-
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
 import "./style/main.scss";
 
 $('a[href^="#"]').on("click", function (event) {
